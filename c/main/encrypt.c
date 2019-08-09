8f57b80a6b8f486419521fda591db3fb9e9e2838
unknown <4bysswalk3r404@gmail.com>
2019-08-08 23:07:13 -0500

    command arguments

--- c/main/encrypt.c
+++ c/main/encrypt.c
@@ -6,10 +6,28 @@
 int main(int argc, char** argv)
 {
     char* infilename = argv[1];
-
     if (access(infilename, F_OK) == -1) {
         printf("%s does not exist or could not be found.\n", infilename);
         return -1;
+    }
+
+    int O_ARG = 0;
+    int S_ARG = 0;
+    for (int i = 0; i < argc; i++) {
+        if (SameStr(argv[i], "-o", 2)) {
+            O_ARG = 1;
+            char* outfilename = (char*)argv[i + 1];
+        } else if (SameStr(argv[i], "-s", 2)) {
+            S_ARG = 1;
+            int seed = atoi(argv[i] + 1);
+        }
+    }
+    if (!(O_ARG)) {
+        char* outfilename = (char*)argv[1];
+    }
+    if (!(S_ARG)) {
+        printf("you must supply a seed/password with -p. killing program.\n");
+        exit(EXIT_FAILURE);
     }
 
     FILE* infile = fopen(infilename, "rb");
@@ -19,6 +37,7 @@ int main(int argc, char** argv)
     rewind(infile);
 
     unsigned char* buffer = (unsigned char*)malloc(sizeof(unsigned char) * bufferSize);
+    void* bufferStart = buffer;
 
     while (!(feof(infile)))
     {
@@ -27,20 +46,9 @@ int main(int argc, char** argv)
     }
 
     fclose(infile);
-    for (int i = 0; i < bufferSize + 1; i++)
-        buffer--;
+    buffer = bufferSize;
 
-    encrypt(buffer, bufferSize, atoi(argv[2]));
-
-    char* outfilename = (char*)malloc(sizeof(char) * 100);
-    SetStr(outfilename, infilename);
-    ConcatStr(outfilename, ".enc");
-    for (int i = 1; access(outfilename, F_OK) != -1; i++)
-    {
-        SetStr(outfilename, infilename);
-        ConcatStr(outfilename, ToStr(i));
-        ConcatStr(outfilename, ".enc");
-    }
+    encrypt(buffer, bufferSize, atoi(seed);
 
     FILE* outfile = fopen(outfilename, "wb");
     fwrite(buffer, bufferSize, 1, outfile);
